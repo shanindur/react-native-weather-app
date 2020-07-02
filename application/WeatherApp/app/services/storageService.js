af3f9ab7bd53050c2b7bb02183a327bf5793cfa8
Shanindu Rajapaksha <shanindu.rajapaksha91@gmail.com>
2020-07-02 17:25:50 +0530

    Created StorageService

---
+++ application/WeatherApp/app/services/storageService.js
@@ -0,0 +1,36 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+const storeData = async (key, data) => {
+	try {
+		await AsyncStorage.setItem(key, JSON.stringify(data));
+	} catch (e) {
+		// saving error
+	}
+};
+
+const getData = async (key) => {
+	try {
+		const value = await AsyncStorage.getItem(key);
+
+		if (null !== value) {
+			return JSON.parse(value);
+		}
+	} catch (e) {
+		// error reading value
+	}
+};
+
+const removeStorage = async (key) => {
+	try {
+		await AsyncStorage.removeItem(key);
+	} catch (e) {
+		// error reading value
+	}
+};
+
+
+export default {
+	storeData,
+	getData,
+	removeStorage
+};
