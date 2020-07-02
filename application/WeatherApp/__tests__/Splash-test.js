038b3eb68ee9d484ea470f0ec9d5fc8d6bca0ce0
Shanindu Rajapaksha <shanindu.rajapaksha91@gmail.com>
2020-07-02 18:04:57 +0530

    Done Jest Testing

---
+++ application/WeatherApp/__tests__/Splash-test.js
@@ -0,0 +1,15 @@
+// __tests__/__snapshot__/Splash-test.js
+import React from 'react';
+import {SplashScreen} from '../app/screens';
+import renderer from 'react-test-renderer';
+
+global.fetch = jest.fn(() => new Promise(resolve => resolve()));
+jest.mock('react-native-gesture-handler', () => {});
+jest.mock('react-native-image-picker', () => {});
+jest.mock('@react-native-community/async-storage', () => {});
+jest.mock('@react-native-community/geolocation', () => {});
+
+test('Snapshot test', () => {
+	const tree = renderer.create(<SplashScreen />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
